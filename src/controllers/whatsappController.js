3c0bdc7fe9f10506273414cf7493c5381bdc0e11
anthony <anthony0813@gmail.com>
2023-11-09 14:45:53 -0500

    added logic in my verifyToken

--- src/controllers/whatsappController.js
+++ src/controllers/whatsappController.js
@@ -1,5 +1,17 @@
 const verifyToken = (req, res) => {
-  res.send("Hola VerifyToken");
+  try {
+    const accessToken = "GHADFGAHD552FGADAR5A12AS";
+    const token = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
+
+    if (challenge !== null && token !== null && token == accessToken) {
+      res.send(challenge);
+    } else {
+      res.status(400).send();
+    }
+  } catch (error) {
+    return res.status(400).json({ message: "Server error", error });
+  }
 };
 
 const receivedMessage = (req, res) => {
